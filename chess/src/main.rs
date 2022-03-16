4f3b917331f664e7088a1fa6f0beb81960dd7f88
Albin Dalbert <albin.dalbert.all@gmail.com>
2022-03-16 11:57:06 +0100

    Update main.rs

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -2,16 +2,17 @@ use std::io;
 
 use eframe::{run_native, epi::App, egui::CentralPanel, NativeOptions};
 
-
 struct ChessGame;
 
 impl App for ChessGame {
     fn update(&mut self, ctx: &eframe::egui::Context, frame: &eframe::epi::Frame) {
         CentralPanel:: default().show(ctx, |ui|{
-            ui.label("Board");
-        });
-
-        
+            ui.label("Just a simple central panel,\n\n 
+            but it's not really in the center\n\n
+            thou, I do like the default size of the window\n\n
+            I think Imma keep that");
+            frame.info();
+        });        
     }
 
     fn name(&self) -> &str {
@@ -22,7 +23,7 @@ impl App for ChessGame {
 
 fn main() {
     println!("Hello, chess!");
-    
+
     let app = ChessGame;
     let win_option = NativeOptions::default();
 
